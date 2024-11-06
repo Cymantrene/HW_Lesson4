c3fbc54f249eb34186818d5d5c12fd98b5f3d40d
Cymantrene <rengenhik@gmail.com>
2024-11-06 12:42:03 +0300

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <windows.h>
+#include <locale.h>
+#include <inttypes.h>
+
+int main()
+{
+    //setlocale(LC_ALL, "en_US.UTF-8");
+    //SetConsoleCP(1251);
+    //SetConsoleOutputCP(1251);
+	int a,b,c,d,e,max;
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+
+    max = a>b ? a : b;
+    max = max>c ? max : c;
+    max = max>d ? max : d;
+    max = max>e ? max : e;
+/*
+    min = a<b ? a : b;
+    min = min<c ? min : c;
+    min = min<d ? min : d;
+    min = min<e ? min : e;
+    sum = max+min;
+*/
+    printf("%d", max);
+    return 0;
+}
+
+
